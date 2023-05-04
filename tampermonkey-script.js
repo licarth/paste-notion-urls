b7f3d6c1a974764351a16f3f36a27e72cee25c06
Thomas Carli <thomascarli@gmail.com>
2023-05-04 07:41:59 +0200

    new button location & style

--- tampermonkey-script.js
+++ tampermonkey-script.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         Well-formatted Notion URLs to your clipboard
 // @namespace    https://gist.github.com/licarth
-// @version      0.3.0
+// @version      0.4.0
 // @description  Adds a button to copy/paste a nicely formatted link to the current Notion page. Paste it in Slack, GitHub, or anywhere that supports text/helm Clipboard items. Paste as value for markdown version.
 // @author       licarth
 // @match        https://www.notion.so/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=notion.so
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
 // @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @grant GM_addStyle
 // @updateURL    https://raw.githubusercontent.com/licarth/paste-notion-urls/main/tampermonkey-script.js
 // @downloadURL  https://raw.githubusercontent.com/licarth/paste-notion-urls/main/tampermonkey-script.js
 // ==/UserScript==
@@ -15,9 +16,16 @@
 /* globals ClipboardItem waitForKeyElements $ */
 
 "use strict";
-const INITIAL_BUTTON_TEXT = "Copy Formatted URL";
+const INITIAL_BUTTON_TEXT = "link";
 
 const IS_DARK_MODE = document.body.classList.contains("dark");
+
+const textColor = IS_DARK_MODE
+  ? "rgba(255, 255, 255, 0.81)"
+  : "rgb(55, 53, 47)";
+const backgroundColorOnHover = IS_DARK_MODE
+  ? "rgba(255, 255, 255, 0.055)"
+  : "rgba(55, 53, 47, 0.08)";
 
 function getPeekPreviewParent(element) {
   return element.closest(".notion-peek-renderer");
@@ -27,12 +35,60 @@ function isWithinPeekPreview(element) {
   return Boolean(getPeekPreviewParent(element));
 }
 
+function createMarkdownIconSVG() {
+  const iconSvg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  const iconPath = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "path"
+  );
+
+  iconSvg.setAttribute("viewBox", "0 0 16 16");
+  iconSvg.classList.add("link");
+  iconSvg.setAttribute(
+    "style",
+    `width: 14px; height: 14px; display: block; flex-shrink: 0; backface-visibility: hidden; margin-left: 0px; color: ${textColor};`
+  );
+
+  iconPath.setAttribute(
+    "d",
+    "M6.345 5h2.1v6.533H6.993l.055-5.31-1.774 5.31H4.072l-1.805-5.31c.04.644.06 5.31.06 5.31H1V5h2.156s1.528 4.493 1.577 4.807L6.345 5zm6.71 3.617v-3.5H11.11v3.5H9.166l2.917 2.916L15 8.617h-1.945z"
+  );
+
+  iconSvg.appendChild(iconPath);
+
+  return iconSvg;
+}
+
 function displayButton(elements) {
   const el = elements[0];
   const peekPreview = isWithinPeekPreview(el);
 
+  const textDiv = document.createElement("div");
+  textDiv.innerHTML = INITIAL_BUTTON_TEXT;
+  textDiv.setAttribute("role", "button");
+  textDiv.setAttribute(
+    "style",
+    `
+user-select: none;
+transition: background 20ms ease-in 0s;
+cursor: pointer;
+display: inline-flex;
+align-items: center;
+flex-shrink: 0;
+white-space: nowrap;
+height: 28px;
+border-radius: 3px;
+font-size: 14px;
+line-height: 1.2;
+min-width: 0px;
+padding-left: 8px;
+margin-right: 2px;
+`
+  );
+
   const linkDiv = document.createElement("div");
-  linkDiv.innerHTML = INITIAL_BUTTON_TEXT;
+  linkDiv.append(createMarkdownIconSVG());
+  linkDiv.append(textDiv);
   linkDiv.setAttribute(
     "id",
     peekPreview ? "copyUrlButtonPeek" : "copyUrlButtonMain"
@@ -40,13 +96,36 @@ function displayButton(elements) {
   linkDiv.setAttribute("role", "button");
   linkDiv.setAttribute(
     "style",
-    "user-select: none; transition: background 20ms ease-in 0s; cursor: pointer; display: inline-flex; align-items: center; flex-shrink: 0; white-space: nowrap; height: 28px; border-radius: 3px; font-size: 14px; line-height: 1.2; min-width: 0px; padding-left: 8px; padding-right: 8px; margin-right: 2px;"
+    `
+user-select: none;
+transition: background 20ms ease-in 0s;
+cursor: pointer;
+display: inline-flex;
+align-items: center;
+flex-shrink: 0;
+white-space: nowrap;
+height: 28px;
+border-radius: 4px;
+font-size: 14px;
+line-height: 1.2;
+min-width: 0px;
+padding-left: 8px;
+padding-right: 4px;
+margin-right: 6px;
+margin-left: auto;
+color: ${textColor};
+display: inline-flex;
+align-items: center;
+`
   );
-  linkDiv.style.color = IS_DARK_MODE
-    ? "rgba(255, 255, 255, 0.81)"
-    : "rgb(55, 53, 47)";
 
-  el.parentNode.appendChild(linkDiv);
+  GM_addStyle(`
+    #copyUrlButtonMain:hover {
+      background-color: ${backgroundColorOnHover} !important;
+    }
+`);
+
+  el.parentNode.insertBefore(linkDiv, el.parentNode.firstChild);
 
   if (peekPreview) {
     document
@@ -71,7 +150,7 @@ function displayButton(elements) {
       ? lastPageTitleDiv.childNodes[1].textContent
       : lastPageTitleDiv.textContent;
 
-    copyLinkToPage(pageEmoji, pageTitle, window.location.href, linkDiv);
+    copyLinkToPage(pageEmoji, pageTitle, window.location.href, textDiv);
   }
 
   function onButtonClickPeek(element) {
@@ -87,13 +166,11 @@ function displayButton(elements) {
     );
     const href = openAsPageThickLogoElement.closest("a")?.href;
 
-    copyLinkToPage(emoji, title, href, linkDiv);
+    copyLinkToPage(emoji, title, href, textDiv);
   }
 }
 
-function copyLinkToPage(emoji, title, href, linkDiv) {
-  const pageLogoAndTitle = (emoji ? `${emoji} ` : "") + title;
-
+function copyToClipboard(pageLogoAndTitle, href) {
   navigator.clipboard.write([
     new ClipboardItem({
       "text/plain": new Blob([`[${pageLogoAndTitle}](${href})`], {
@@ -109,10 +186,16 @@ function copyLinkToPage(emoji, title, href, linkDiv) {
       ),
     }),
   ]);
+}
 
-  linkDiv.textContent = "✅ Copied!";
+function copyLinkToPage(emoji, title, href, textDiv) {
+  const pageLogoAndTitle = (emoji ? `${emoji} ` : "") + title;
+
+  copyToClipboard(pageLogoAndTitle, href);
+
+  textDiv.textContent = "✅ Copied!";
   setTimeout(() => {
-    linkDiv.textContent = INITIAL_BUTTON_TEXT;
+    textDiv.textContent = INITIAL_BUTTON_TEXT;
   }, 1500);
 
   console.log(`Copied '${pageLogoAndTitle}' to clipboard !`);
